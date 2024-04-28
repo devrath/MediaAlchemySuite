5b9218b53bc3d945d23c00fafc2915c77553d3f7
Devrath <devrath.dev595@gmail.com>
2024-04-28 09:17:28 +0530

    added

--- code/app/src/main/java/com/istudio/media3/demos/audio/AudioDemoScreen.kt
+++ code/app/src/main/java/com/istudio/media3/demos/audio/AudioDemoScreen.kt
@@ -2,8 +2,13 @@ package com.istudio.media3.demos.audio
 
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.lifecycle.viewmodel.compose.viewModel
 
 @Composable
 fun AudioDemoScreen () {
+
+    val vm: AudioDemoVm = viewModel()
+
+
     Text(text = "Hello")
 }
---
+++ code/app/src/main/java/com/istudio/media3/demos/audio/AudioDemoVm.kt
@@ -0,0 +1,8 @@
+package com.istudio.media3.demos.audio
+
+import androidx.lifecycle.ViewModel
+
+class AudioDemoVm : ViewModel() {
+
+
+}
