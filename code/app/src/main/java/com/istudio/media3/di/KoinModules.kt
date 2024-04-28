fd2c32d31f4b4b0413d23737952774293880cdcb
Devrath <devrath.dev595@gmail.com>
2024-04-28 13:01:53 +0530

    added new changes

--- code/app/src/main/java/com/istudio/media3/di/KoinModules.kt
+++ code/app/src/main/java/com/istudio/media3/di/KoinModules.kt
@@ -1,6 +1,7 @@
 package com.istudio.media3.di
 
 import androidx.media3.exoplayer.ExoPlayer
+import com.istudio.media3.demos.audio.ui.AudioDemoVm
 import com.istudio.media3.main.MainViewModel
 import com.istudio.media3.main.selection.SelectionScreenViewModel
 import org.koin.androidx.viewmodel.dsl.viewModel
@@ -9,6 +10,7 @@ import org.koin.dsl.module
 val viewModelModules = module {
     viewModel { MainViewModel() }
     viewModel { SelectionScreenViewModel() }
+    viewModel { AudioDemoVm(get()) }
 }
 
 val mediaModules = module {
