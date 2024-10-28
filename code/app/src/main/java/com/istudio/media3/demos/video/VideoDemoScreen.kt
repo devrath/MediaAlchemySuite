25a9e0aeb79b6484860d7ca922c5e97fcf6583a5
Devrath <devrath.dev595@gmail.com>
2024-10-28 14:26:35 +0530

    playing sample

--- code/app/src/main/java/com/istudio/media3/demos/video/VideoDemoScreen.kt
+++ code/app/src/main/java/com/istudio/media3/demos/video/VideoDemoScreen.kt
@@ -1,14 +1,93 @@
 package com.istudio.media3.demos.video
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.viewinterop.AndroidView
 import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.media3.exoplayer.ExoPlayer
+import androidx.media3.ui.PlayerView
+import com.istudio.media3.data.DataSamples
+import org.koin.androidx.compose.koinViewModel
 
 @Composable
-fun VideoDemoScreen () {
+fun VideoDemoScreen (viewModel: VideoDemoVm = koinViewModel()) {
 
-    val vm: VideoDemoVm = viewModel()
+    val player by viewModel.playerState.collectAsState()
+
+    LaunchedEffect(Unit) {
+        // Initialize the ExoPlayer only once
+        viewModel.initializePlayer()
+    }
+
+    DisposableEffect(Unit) {
+        // Call this when the composable is destroyed
+        onDispose {
+            viewModel.savePlayerState()
+            viewModel.releasePlayer()
+        }
+    }
+
+    Column {
+        Media3AndroidView(player)
+        PlayerControls(player)
+    }
+
+}
 
 
-    Text(text = "Hello")
+@Composable
+fun Media3AndroidView(player: ExoPlayer?) {
+    AndroidView(
+        modifier = Modifier.fillMaxWidth(),
+        factory = { context ->
+            PlayerView(context).apply {
+                this.player = player
+            }
+        },
+        update = { playerView ->
+            playerView.player = player
+        }
+    )
+}
+
+@Composable
+fun PlayerControls(player: ExoPlayer?) {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(24.dp),
+        horizontalAlignment = Alignment.Start,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Button(onClick = { player?.playWhenReady = true }) {
+            Text("Play")
+        }
+        Button(onClick = { player?.playWhenReady = false }) {
+            Text("Pause")
+        }
+
+        Button(onClick = {
+            player?.seekTo(player.currentPosition - 10_000) // Seek backward 10 seconds
+        }) {
+            Text("Seek -10s")
+        }
+        Button(onClick = {
+            player?.seekTo(player.currentPosition + 10_000) // Seek forward 10 seconds
+        }) {
+            Text("Seek +10s")
+        }
+    }
 }
--- code/app/src/main/java/com/istudio/media3/di/KoinModules.kt
+++ code/app/src/main/java/com/istudio/media3/di/KoinModules.kt
@@ -2,6 +2,7 @@ package com.istudio.media3.di
 
 import android.content.Context
 import androidx.media3.exoplayer.ExoPlayer
+import com.istudio.media3.demos.video.VideoDemoVm
 import com.istudio.media3.main.MainViewModel
 import com.istudio.media3.main.selection.SelectionScreenViewModel
 import org.koin.android.ext.koin.androidContext
@@ -16,6 +17,7 @@ val mediaModules = module {
 val viewModelModules = module {
     viewModel { MainViewModel() }
     viewModel { SelectionScreenViewModel() }
+    viewModel { VideoDemoVm(exoPlayer = get()) }
 }
 
 /**
