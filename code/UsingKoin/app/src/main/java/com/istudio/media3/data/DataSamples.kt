536836173ff37019cedbeeb01a7cacbe2043b136
Devrath <devrath.dev595@gmail.com>
2025-07-18 22:34:36 +0530

    moved the code

--- code/UsingKoin/app/src/main/java/com/istudio/media3/data/DataSamples.kt
+++
@@ -1,5 +0,0 @@
-package com.istudio.media3.data
-
-object DataSamples {
-    const val SAMPLE_ONE_VIDEO_MP4 = "https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4"
-}
--- code/UsingKoin/app/src/main/java/com/istudio/media3/main/selection/SelectionScreenViewModel.kt
+++
@@ -1,9 +0,0 @@
-package com.istudio.media3.main.selection
-
-import androidx.lifecycle.ViewModel
-
-class SelectionScreenViewModel : ViewModel()  {
-
-
-
-}
